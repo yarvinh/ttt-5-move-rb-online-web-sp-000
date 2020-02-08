87f57f21e218841576049542b828834918c3f2c8
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-08 23:47:43 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -15,6 +15,6 @@ def input_to_index(user_input)
 end
 
 def move(board,position,player = "x")
-  board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 end
-# move(board, 0, "X")
+display_board(board)
