05bb71606212d754c6bd6afa9252f855a3cdb216
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-08 23:15:16 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,18 +7,11 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-display_board([" "," "," "," "," "," "," "," "," "])
+# display_board([" "," "," "," "," "," "," "," "," "])
 
 def input_to_index(user_input)
   display_board(user_input)
-  # display_board(user_input[1 - 1])
-  # display_board(user_input[1 - 1])
-  # display_board(user_input[1 - 1])
-  # display_board(user_input[1 - 1])
-  # display_board(user_input[1 - 1])
-  # display_board(user_input[1 - 1])
-  # display_board(user_input[1 - 1])
-
+  return user_input
 end
 input_to_index(["1".to_i,"2".to_i,"3".to_i,"4".to_i,"5".to_i,"6".to_i,"7".to_i,"8".to_i,"9".to_i])
 
