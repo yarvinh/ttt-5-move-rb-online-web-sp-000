7545c7bb173add42e1a44d65560088710f9b73d8
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-08 23:33:29 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,21 +6,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
-# display_board([" "," "," "," "," "," "," "," "," "])
+
 
 def input_to_index(user_input)
 
-  user_input.to_i
+  index_number = user_input.to_i
+  index_number - 1
+  return index_number
+
 
 end
-# input_to_index(["1".to_i,"2".to_i,"3".to_i,"4".to_i,"5".to_i,"6".to_i,"7".to_i,"8".to_i,"9".to_i])
-
-#display_board([" "," "," "," "," "," "," "," "," "])
-
-
-# def move(board_array,index,player = "x")
-#   board = display_board(board)
-#   index = input_to_index(user_input)
-#   player
-# end
