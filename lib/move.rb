86b60b15343bda17683a1115abe8233ffd853278
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-08 22:56:40 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,7 +10,14 @@ end
 display_board([" "," "," "," "," "," "," "," "," "])
 
 def input_to_index(user_input)
-  display_board(user_input)
+  display_board(user_input[1 - 1])
+  # display_board(user_input[1 - 1])
+  # display_board(user_input[1 - 1])
+  # display_board(user_input[1 - 1])
+  # display_board(user_input[1 - 1])
+  # display_board(user_input[1 - 1])
+  # display_board(user_input[1 - 1])
+  # display_board(user_input[1 - 1])
 
 end
 input_to_index(["1".to_i,"2".to_i,"3".to_i,"4".to_i,"5".to_i,"6".to_i,"7".to_i,"8".to_i,"9".to_i])
